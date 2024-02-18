cb308a418fd43a54141a25a179d6a54b0deff8ce
naoki_gonda <naoki@greff.co.jp>
2024-02-18 19:54:45 +0900

    テスト時にDBをテストDBに変更できるようにした

--- app/resource/depends/depends.py
+++ app/resource/depends/depends.py
@@ -1,8 +1,12 @@
 from injector import Injector, inject
-from app.db.db import AppConfig
+from app.db.db import AppConfig, TestAppConfig
 
-injection = Injector([AppConfig()])
+di = Injector([AppConfig()])
+
+async def update_injector(_class):
+    global di
+    di = Injector([_class])
 
 @inject
 def get_di_class(_class):
-    return injection.get(_class)
+    return di.get(_class)
